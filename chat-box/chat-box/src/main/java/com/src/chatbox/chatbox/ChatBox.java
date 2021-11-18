ddd7e328d9fd8cd2cb54650a244daadf6695e19d
shanlyds <94114301+shanlyds@users.noreply.github.com>
2021-11-18 12:45:01 +0000

    Create ChatBox.java

---
+++ chat-box/chat-box/src/main/java/com/src/chatbox/chatbox/ChatBox.java
@@ -0,0 +1,23 @@
+/**
+ * Chat box 
+ *
+ * @author LydiaL
+ */
+package com.src.chatbox.chatbox;
+
+public class ChatBox {
+
+	private String userName;
+
+	public ChatBox(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
